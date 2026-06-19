7b4095c0d76db4a2a2e5a3eebaa94d9c9058acee
Oscar Spruit <oscar.spruit@adyen.com>
2026-06-19 14:47:14 +0200

    Implement manual intent handling in example implementations
    COSDK-1119

--- example-app/src/main/java/com/adyen/checkout/example/ui/v6/V6SessionsViewModel.kt
+++ example-app/src/main/java/com/adyen/checkout/example/ui/v6/V6SessionsViewModel.kt
@@ -8,6 +8,7 @@
 
 package com.adyen.checkout.example.ui.v6
 
+import android.content.Intent
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -47,6 +48,8 @@ internal class V6SessionsViewModel @Inject constructor(
 ) : ViewModel() {
 
     private lateinit var checkoutContext: CheckoutContext.Sessions
+
+    private var checkoutController: CheckoutController? = null
 
     var uiState by mutableStateOf<V6UiState>(V6UiState.Loading)
 
@@ -153,7 +156,21 @@ internal class V6SessionsViewModel @Inject constructor(
                 )
             },
             coroutineScope = viewModelScope,
-        )
+        ).also {
+            checkoutController = it
+        }
+    }
+
+    fun onNewIntent(intent: Intent) {
+        val returnUrl = savedStateHandle.get<String>(V6Activity.RETURN_URL_EXTRA) ?: return
+        if (intent.data?.toString()?.startsWith(returnUrl) == true) {
+            checkoutController?.handleReturn(intent)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        checkoutController = null
     }
 
     companion object {
